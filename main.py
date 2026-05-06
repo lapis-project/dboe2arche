f7fe8a66aaadfb46e5b131e34a6a3d23c0b8b5bb
csae8092 <p.andorfer@gmail.com>
2026-05-06 14:08:06 +0200

    feat: better description

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ DBOE_XMLS = "/home/csae8092/repos/dboe/dboe_orig_xml/*.xml"
 
 files_to_ingest = sorted(glob.glob(DBOE_XMLS))
 
-for x in tqdm(files_to_ingest[30:40]):    
+for x in tqdm(files_to_ingest):    
     f_name = os.path.basename(x)
     subj = URIRef(f"{TOP_COL_URI}/{f_name}")
     g.add((subj, RDF.type, ACDH["Resource"]))
