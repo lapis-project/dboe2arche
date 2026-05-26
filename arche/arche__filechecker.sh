0995d2b7f447f875ad82dfcecd6428c7d29d65ac
csae8092 <p.andorfer@gmail.com>
2026-05-26 08:01:21 +0200

    feat: checks in filechecker results

--- arche/arche__filechecker.sh
+++ arche/arche__filechecker.sh
@@ -1,13 +1,11 @@
 #/bin/bash
 echo "run filechecker for for ${TOPCOLID}"
 rm -rf ${PWD}/fc_out && mkdir ${PWD}/fc_out
-mv ${PWD}/to_ingest/arche.ttl ${PWD}/to_ingest/arche.txt 
 docker run \
   --rm \
   --network="host" \
   -v ${PWD}/fc_out:/reports \
-  -v ${PWD}/to_ingest:/data \
+  -v /home/csae8092/repos/dboe/dboe_orig_xml/:/data \
   --entrypoint arche-filechecker \
   acdhch/arche-ingest \
   --overwrite --skipWarnings /data /reports
-mv ${PWD}/to_ingest/arche.txt ${PWD}/to_ingest/arche.ttl
