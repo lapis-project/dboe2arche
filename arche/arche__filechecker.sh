7a19554bdd4a82873c7a41e0e78a5d348b949725
csae8092 <p.andorfer@gmail.com>
2026-05-26 08:12:02 +0200

    chore: changed path for filechecker script

--- arche/arche__filechecker.sh
+++ arche/arche__filechecker.sh
@@ -5,7 +5,7 @@ docker run \
   --rm \
   --network="host" \
   -v ${PWD}/fc_out:/reports \
-  -v /home/csae8092/repos/dboe/dboe_orig_xml/:/data \
+  -v ${PWD}/data:/data \
   --entrypoint arche-filechecker \
   acdhch/arche-ingest \
   --overwrite --skipWarnings /data /reports
