418e04b3becfb90c841d4a7afa520d015e3f2bbb
csae8092 <p.andorfer@gmail.com>
2026-06-03 12:14:46 +0200

    feat(tustep): (re)added filchecker output for tustep sample data

--- arche/arche__filechecker_tustep.sh
+++ arche/arche__filechecker_tustep.sh
@@ -3,8 +3,8 @@ echo "run filechecker for for ${TOPCOLID}"
 docker run \
   --rm \
   --network="host" \
-  -v ${PWD}/fc_out_tustep_bagit:/reports \
-  -v /home/csae8092/Schreibtisch/tustep/foo:/data \
+  -v ${PWD}/fc_out_tustep:/reports \
+  -v /home/csae8092/Schreibtisch/tustep/tustep01/data/754:/data \
   --entrypoint arche-filechecker \
   acdhch/arche-ingest \
   --overwrite --skipWarnings /data /reports
