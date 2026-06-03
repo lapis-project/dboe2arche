170abb42e074ac0ed9a206f2c9b786290d51b351
csae8092 <p.andorfer@gmail.com>
2026-06-03 12:13:05 +0200

    feat(bagit): adds filechecker report for bagit check

--- arche/arche__filechecker_tustep.sh
+++ arche/arche__filechecker_tustep.sh
@@ -1,11 +1,10 @@
 #/bin/bash
 echo "run filechecker for for ${TOPCOLID}"
-rm -rf ${PWD}/fc_out && mkdir ${PWD}/fc_out
 docker run \
   --rm \
   --network="host" \
-  -v ${PWD}/fc_out_tustep:/reports \
-  -v /home/csae8092/Schreibtisch/tustep/tustep01/data/120:/data \
+  -v ${PWD}/fc_out_tustep_bagit:/reports \
+  -v /home/csae8092/Schreibtisch/tustep/foo:/data \
   --entrypoint arche-filechecker \
   acdhch/arche-ingest \
   --overwrite --skipWarnings /data /reports
