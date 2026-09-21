7d88ce89549eaeb73b58ff762a95f890d70f36c4
csae8092 <p.andorfer@gmail.com>
2026-09-21 09:50:56 +0200

    adds some analysis scripts

--- analysis/defs.py
+++ analysis/defs.py
@@ -4,23 +4,37 @@ import json
 from acdh_tei_pyutils.tei import TeiReader
 from acdh_tei_pyutils.utils import any_xpath, get_xmlid
 
-more_than_one_def = {}
-files = sorted(glob.glob("./data/*.xml"))
+try:
+    from analysis.utils import node_to_json
+except ModuleNotFoundError:
+    from utils import node_to_json
+
+items = {}
+files = sorted(glob.glob("./data/*.xml"))[0:20]
+attribute_names = set()
 for x in files:
     doc = TeiReader(x)
     print(f"processing {x}")
-    for y in doc.any_xpath(".//tei:cit[./tei:def[2]]"):
+    for y in doc.any_xpath(".//tei:entry[./tei:cit/tei:def]"):
         try:
             xml_id = get_xmlid(y)
         except KeyError:
             continue
-        more_than_one_def[xml_id] = []
-        for tei_def in any_xpath(y, ".//tei:def"):
-            item = {"text": tei_def.text}
-            item.update(tei_def.attrib)
-            more_than_one_def[xml_id].append(item)
+        items[xml_id] = []
+        for node in any_xpath(y, "./tei:cit/tei:def"):
+            items[xml_id].append(node_to_json(node))
+
+all_keys = set()
+
+# go through def items and save their first-level key names
+for x in items.values():
+    for y in x:
+        all_keys.update(y)
 
 with open("defs.json", "w", encoding="utf-8") as fp:
-    json.dump(more_than_one_def, fp, ensure_ascii=False, indent=4)
+    json.dump(items, fp, ensure_ascii=False, indent=4)
 
-print(len(more_than_one_def))
+with open("defs_attribute_names.json", "w", encoding="utf-8") as fp:
+    json.dump(sorted(all_keys), fp, ensure_ascii=False, indent=4)
+
+print(len(items))
